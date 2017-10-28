70d48644f04cc1b4a07fa2fe5109c15c5f3c2237
Patrick McCarty <patrickthesailorman@gmail.com>
2017-10-28 15:03:29 +0000

    Add files water and Hello

---
+++ water.c
@@ -0,0 +1,17 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    int n;
+    do
+    {
+        printf("How many minutes is your shower?\n");
+        n = get_int(); // User inputs minutes
+    }
+    while (n < 0); // Validate minutes
+
+    int x = n * 12; // Bottles of Water
+
+    printf("Your %i minute shower, uses %i bottles of water\n", n, x);
+}
