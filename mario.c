ff29b19731f40bb46ea58c481a1331e5e4c238db
Patrick McCarty <patrickthesailorman@gmail.com>
2017-10-28 16:57:05 +0000

    Add Mario

---
+++ mario.c
@@ -0,0 +1,30 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    int height;
+    do
+    {
+        printf("How many steps will Mario climb?\n");
+        height = get_int(); // User inputs steps
+    }
+    while (height < 0 || height > 23); // Validate steps
+
+    for (int i = 0; i < height; i++) // Loop counter
+    {
+
+
+        for (int x = 0; x < height - i - 1; x++) //Loop decreasing spaces
+        {
+            printf(" ");
+        }
+
+        for (int y = 0; y < 2 + i; y++) // Loop increasing #'s'
+        {
+            printf("#");
+        }
+
+        printf("\n");
+    }
+}
