6e826fd9300e21352fd02e1c53497c9998b76d8b
Patrick McCarty <patrickthesailorman@gmail.com>
2017-10-29 21:38:59 +0000

    Add greedy

---
+++ greedy.c
@@ -0,0 +1,40 @@
+#include <cs50.h>
+#include <stdio.h>
+#include <math.h>
+
+int main(void)
+{
+    float change;
+    int coins = 0;
+    int cents;
+    do
+    {
+        printf("How much change is owed?\n$");
+        change = get_float(); // User input
+    }
+    while (change < 0.00); // Validate input is a float
+
+     cents = round (change * 100); // Convert float to int
+
+    while (cents >= 25) // Will it take a quater, how many
+        {
+            coins ++;
+            cents -=25;
+        }
+    while (cents >= 10) // Will it take a dime, how many
+        {
+            coins ++;
+            cents -=10;
+        }
+    while (cents >= 5) // Will it take a nickel, how many
+        {
+            coins ++;
+            cents -=5;
+        }
+    while (cents >= 1) // Remainder in pennies
+        {
+            coins ++;
+            cents -=1;
+        }       
+        printf("Total coins:%i\n", coins); // return total number of coins used to make change
+}
