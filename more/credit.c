3b2c84e455fdc2b470a3e9d26807dfa9d08df78d
Patrick McCarty <patrickthesailorman@gmail.com>
2018-04-03 20:18:38 +0000

    Add credit

---
+++ more/credit.c
@@ -0,0 +1,14 @@
+#include <cs50.h>
+#include <stdio.h>
+
+// Validate cc number
+
+int main(void)
+{
+    int cc;
+    do
+    {
+        printf("Enter credit card number:\n");
+        cc = get_long_long(); // User enters cc number
+    }
+}
